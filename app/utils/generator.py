da6062033460f4fe32e1d99e66bc765fddc7ba48
Orangeliquid <hardym2020111@gmail.com>
2025-07-04 03:09:49 -0400

    Import generator.py from password_generator project to generate random passwords in create_new_entry_form, Import strength.py for strength evaluation of current and generated password in the future

---
+++ app/utils/generator.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+from app.models.password_options import PasswordOptions
+
+UPPERCASE = string.ascii_uppercase
+LOWERCASE = string.ascii_lowercase
+NUMBERS = string.digits
+SYMBOLS = string.punctuation
+
+
+def generate_password(options: PasswordOptions) -> str:
+    characters = ""
+
+    characters += UPPERCASE
+    characters += LOWERCASE
+
+    if options.include_numbers:
+        characters += NUMBERS
+    if options.include_symbols:
+        characters += SYMBOLS
+
+    if not characters:
+        raise ValueError("No character types selected for password generation.")
+
+    return "".join(random.choice(characters) for i in range(options.length))
