e2918d46b5e9cb8f71099a9367458469959169f5
Orangeliquid <hardym2020111@gmail.com>
2025-08-27 03:56:33 -0400

    Add update_entry function for to update db entry

--- app/crud_vault.py
+++ app/crud_vault.py
@@ -1,6 +1,6 @@
 from app.database import SessionLocal
 from app.models.vault_entry import VaultEntry
-from app.models.schemas import VaultEntryCreate
+from app.models.schemas import VaultEntryCreate, VaultEntryUpdate
 
 
 def create_entry(entry_data: VaultEntryCreate):
@@ -27,3 +27,21 @@ def get_all_entries():
     entries = db.query(VaultEntry).order_by(VaultEntry.created_at.desc()).all()
     db.close()
     return entries
+
+
+def update_entry(entry_id: int, update_data: VaultEntryUpdate):
+    db = SessionLocal()
+    try:
+        entry = db.query(VaultEntry).filter(VaultEntry.id == entry_id).first()
+        if entry is None:
+            return None
+
+        for field, value in update_data.model_dump(exclude_unset=True).items():
+            setattr(entry, field, value)
+
+        db.commit()
+        db.refresh(entry)
+        return entry
+
+    finally:
+        db.close()
